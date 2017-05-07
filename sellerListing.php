677cf1c8c3541118c918368846a07e82c54ae7a3
Lihan Zhang <lihan.bibi.zhang@gmail.com>
2017-05-06 22:58:42 -0400

    Most features implemented

--- sellerListing.php
+++ sellerListing.php
@@ -3,20 +3,47 @@
 
     require_once "support.php";
 
-    $css = "<link rel=\"stylesheet\" href=\"css\newListing.css\" />
-            ";
-
     $body = <<<EOBODY
-        <div class="form-format">
-            <form action="success.php">
-                <h1>Create a listing:</h1>
+        <form action="success.php">
+            <h1>Create a listing:</h1>
+            <div class="col-lg-2 col-lg-offset-5 form-row">
                 <span class="label-text">What are you selling?</span>
-                <input type="text">
-                <br>
-                <br>
-                <input type="submit">
-            </form>
-        </div>
+                <input type="text" name="name" class="form-control">
+            </div>
+            <br>
+            <div class="col-lg-2 col-lg-offset-5 form-row">
+                <span class="label-text">How many?</span>
+                <input type="text" name="quantity" class="form-control">
+            </div>
+            <br>
+            <div class="col-lg-2 col-lg-offset-5 form-row">
+                <span class="label-text">How much per unit?</span>
+                <div class="input-group">
+                  <span class="input-group-addon">$</span>
+                  <input type="text" name="price" class="form-control">
+                </div>
+            </div>
+            <br>
+            <div class="form-row">
+                <div class="radio">
+                  <label><input type="radio" name="optradio">Option 1</label><br>
+                </div>
+                <div class="radio">
+                  <label><input type="radio" name="optradio">Option 2</label><br>
+                </div>
+                <div class="radio">
+                  <label><input type="radio" name="optradio">Option 3</label>
+                </div>
+            </div>
+            <div class="col-lg-4 col-lg-offset-4">
+                <div class="form-group">
+                  <label for="comment">Comment:</label>
+                  <textarea class="form-control" rows="5" name="description" id="comment"></textarea>
+                </div>
+                <input type="submit" class="btn btn-primary btn-sm" value="Submit Listing">
+            </div>
+            <br>
+        </form>
 
 EOBODY;
 
--- support.php
+++ support.php
@@ -8,10 +8,6 @@ function generatePage($body, $title) {
         <meta charset="utf-8" />
         <title>$title</title>
 
-        <link rel="stylesheet" href="css\style.css" />
-        <link rel="stylesheet" href="css\sellerListing.css" />
-        <link rel="stylesheet" href="css\signUpLogin.css" />
-
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
@@ -21,6 +17,10 @@ function generatePage($body, $title) {
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+        
+        <link rel="stylesheet" href="css\style.css" />
+        <link rel="stylesheet" href="css\sellerListing.css" />
+        <link rel="stylesheet" href="css\signUpLogin.css" />
     </head>
 
     <body>

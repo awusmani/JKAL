5d2759f193d504ac3a876e0f99986ae489ec565f
awusmani <abdulusmani96@gmail.com>
2017-05-06 20:39:46 -0400

    add account to database

--- signUpConfirm.php
+++ signUpConfirm.php
@@ -1,5 +1,26 @@
 <?php
 
-	echo "Test";
+	include "support.php";
+	include "accountsDBLogin.php";
+
+	$database = new mysqli($host, $user, $password, $database);
+	if ($database->connect_error) {
+		die($database->connect_error);
+	}
+	
+	/* Query */
+
+	$phashed = password_hash("".trim($_POST['pass'])."", PASSWORD_DEFAULT);
+
+	$query = "insert into accounts values('')";
+			
+	/* Executing query */
+	$result = $database->query($query);
+	if (!$result) {
+		die("Insertion failed: " . $database->error);
+	}
+	
+	/* Closing connection */
+	$database->close();
 
 ?>

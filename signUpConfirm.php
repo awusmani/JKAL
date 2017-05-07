be2fe93d9a0e4a234b6c993eaf45913b1ac741af
awusmani <abdulusmani96@gmail.com>
2017-05-06 22:25:11 -0400

    sign up confirm fix

--- signUpConfirm.php
+++ signUpConfirm.php
@@ -9,13 +9,10 @@
 		die($database->connect_error);
 	}
 
-<<<<<<< HEAD
 
-
-=======
 	$user = sanitize_string($database, trim($_POST["username"]));
 	$email = sanitize_string($database, trim($_POST["email"])); 
->>>>>>> refs/remotes/origin/master
+
 	$phashed = password_hash("".trim($_POST['password'])."", PASSWORD_DEFAULT);
 	$first = sanitize_string($database, trim($_POST["firstName"]));
 	$last = sanitize_string($database, trim($_POST["lastName"]));

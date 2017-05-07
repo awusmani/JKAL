e209c3c26328c270bb3f2c5b198b372ba0f1cdcc
Justin Chan <justinhchann@gmail.com>
2017-05-06 20:05:12 -0400

    google font, main background, jquery

--- support.php
+++ support.php
@@ -18,6 +18,7 @@ function generatePage($body, $title) {
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
     </head>
 
     <body>

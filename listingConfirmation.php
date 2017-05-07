4dd74bfd1e6b4454d750b8b60f54a9ab424477b3
KWoo13 <giyoon2@hotmail.com>
2017-05-06 22:54:56 -0400

    page fix

--- listingConfirmation.php
+++ listingConfirmation.php
@@ -1,26 +1,28 @@
 <?php
     declare(strict_types=1);
-    require_once("accountsDBLogin.php");
-    require_once("support.php"); 
+	require_once "support.php";
+	require_once "accountsDBLogin.php";
+		
+	$database = new mysqli($host, $user, $password, $database);
+	if ($database->connect_error) {
+		die($database->connect_error);
+	}
 
-    
-    $db_connection = new mysqli($host, $user, $password, $database); 
-    if ($db_connection->connect_error) { 
-        die($db_connection->connect_error); 
-    }
+	
 
-    function sanitize_string($db_connection, $string) {
+	$bottomPart = <<<EOBODY
+        <p> Hello </p>
+EOBODY;
+
+	function sanitize_string($db_connection, $string) {
         if (get_magic_quotes_gpc()) { 
             $string = stripslashes($string); 
         } 
         return htmlentities($db_connection->real_escape_string($string)); 
     }
 
-    $bottomPart <<< EOBODY
-    <p> Hello </p>
-EOBODY;
-
     $body = $bottomPart;
-    $page = generatePage($body,"Add Listing");
-    echo $page;
+	$page = generatePage($body, "JKAL- Welcome");
+	echo $page;
+
 ?>

455853ab197d3338d42399471e7d2557abd86130
KWoo13 <giyoon2@hotmail.com>
2017-05-08 17:28:56 -0400

    decrement item

--- deleteItem.php
+++ deleteItem.php
@@ -6,5 +6,21 @@ function decrementItem($id) {
     $database = "shopdb";
 
     $db_connection = new mysqli($host, $user, $passwordk, $database);
+
+    if ($db_connection->connect_error) {
+        die($db_connection->connect_error);
+    } else {
+        $query = "update items set quantity=quantity-1 where id='{$id}'";
+    
+        $result = $db_connection->query($query);
+
+        if(!$result) {
+            die("Query failed: ".$db_connection->error);
+        } else {
+            $query2 = "delete from items where quantity=0";
+
+            $delete = $db_connection->query($query);
+        }
+    }
 }
 ?>

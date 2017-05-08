592863533980d37ae5bd1dc12d6423455794aa30
awusmani <abdulusmani96@gmail.com>
2017-05-08 18:15:18 -0400

    add to cart button works now

--- js/cartScript.js
+++ js/cartScript.js
@@ -1,39 +1,27 @@
 
 
 function request_access($this){
-    var request_data = $this.id;
-    alert();
-    $.post( "request_access",{ request_data: request_data},function(json) {
-        
-         $("#request-access").hide();
-         console.log("requested access complete");
-    })
+    let request_data = $this.id;
+    
+    let dataString = 'lang=' + request_data;
+
+
+    $.ajax({
+
+    	type: "POST",
+    	url: "AddToCart.php",
+    	data: dataString,
+    	cache: false,
+    	success: function(response){
+
+    		location.reload();
+    	}
+
+    });
+
+    return false;
+    
 }
 
-function runAjax(){
 
-	return
-
-	$.ajax({
-
-		type : 'POST',
-		url  : 'AddToCart.php',
-		data : data,
-			success :  function(response)
-			{
-				if(response=="Login Success")
-				{
-
-					submit.submit();
-
-				}
-			else{
-
-					error.innerHTML = "Invalid Login.";
-
-				}
-			}
-		});
-
-}
 
